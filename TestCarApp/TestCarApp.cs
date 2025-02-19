eda4493356f0a56f676f99e892eed765c9738be1
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-02-19 12:30:18 +0800

    Add test method

---
+++ TestCarApp/TestCarApp.cs
@@ -0,0 +1,19 @@
+﻿namespace TestCarApp
+{
+    [TestClass]
+    public sealed class TestCarApp
+    {
+        [TestMethod]
+        public void TestHeader()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                CarApp.Program.Header("Test Header");
+
+                string expected = "Test Header" + Environment.NewLine + "===========" + Environment.NewLine + Environment.NewLine;
+                Assert.AreEqual(expected, sw.ToString());
+            }
+        }
+    }
+}
